d4910c759a96ba0b4f86071bc2d023b067d19f17
Vítor Rodrigues Tomé <vitorr661@gmail.com>
2023-11-19 13:12:48 -0300

    ReadString protege contra a leitura de input buffers maiores do que o tamanho limite da string

--- ex.c
+++ ex.c
@@ -2,111 +2,118 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct ex
-{
+typedef struct tPessoa {
     char nome[100];
     char dataNascimento[11];
     char cpf[15];
 } tPessoa;
 
-void DesalocaPessoas(tPessoa** pessoas, int nPessoas) {
-    for (int pessoa = 0; pessoa < nPessoas; pessoa++) {
-        free(pessoas[pessoa]);
+void DesalocaPessoas(tPessoa **pessoas, int nPessoas) {
+    for (int i = 0; i < nPessoas; i++) {
+        free(pessoas[i]);
     }
     free(pessoas);
 }
 
-tPessoa** LePessoasInput(int* nPessoas) {
-    tPessoa **pessoas = (tPessoa**) malloc(sizeof(tPessoa *));
- 
-    char controle = 0;
+char *ReadString(const char *prompt, char *dest, size_t size) {
+    size_t i = 0;
+    int c;
 
-    while (1)
-    {
-        (*nPessoas)++;
-        pessoas = (tPessoa **) realloc(pessoas, sizeof(tPessoa*) * (*nPessoas));
-        pessoas[(*nPessoas) - 1] = (tPessoa *) calloc(1, sizeof(tPessoa));
-        
-        printf("----------- NOVA PESSOA -----------\n");
-        printf("Nome: ");
-        scanf("%[^\n]%*c", pessoas[(*nPessoas)-1]->nome);
-        printf("Data de nascimento: ");
-        scanf("%[^\n]%*c", pessoas[(*nPessoas)-1]->dataNascimento);
-        printf("CPF: ");
-        scanf("%[^\n]%*c", pessoas[(*nPessoas)-1]->cpf);
-        while(1){
-            printf("\nDeseja adicionar outra pessoa? [s/n] ");
-            scanf("%c%*c", &controle);
-            printf("\n");
-            if ( controle == 'n' || controle == 'N' ) break;
-            else if ( controle == 's' || controle == 'S' ) break;
+    printf("%s: ", prompt);
+    fflush(stdout);
+    while ((c = getchar()) != EOF && c != '\n') {
+        if (i + 1 < size) {
+            dest[i++] = (char)c;
         }
-        if ( controle == 'n' || controle == 'N' ) break;   
     }
+    if (size) {
+        dest[i] = '\0';
+    }
+    printf("\n");
+    // return dest unless at end of file
+    return (c == EOF && i == 0) ? NULL : dest;
+}
 
+tPessoa **LePessoasInput(int *nPessoas_ptr) {
+    tPessoa **pessoas = NULL;
+    int nPessoas = 0;
+
+    for (;;) {
+        tPessoa pessoa = { 0 };
+
+        printf("----------- NOVA PESSOA -----------\n");
+        if (!ReadString("Nome", pessoa.nome, sizeof pessoa.nome)
+        ||  !ReadString("Data de nascimento", pessoa.dataNascimento, sizeof pessoa.dataNascimento)
+        ||  !ReadString("CPF", pessoa.cpf, sizeof pessoa.cpf)) {
+            /* input failure */
+            break;
+        }
+        pessoas = (tPessoa **)realloc(pessoas, sizeof(tPessoa *) * (nPessoas + 1));
+        pessoas[nPessoas] = (tPessoa *)calloc(sizeof(tPessoa), 1);
+        *(pessoas[nPessoas]) = pessoa;
+        nPessoas += 1;
+
+        char controle = 'n';
+        for (;;) {
+            printf("Deseja adicionar outra pessoa? [s/n] ");
+            scanf(" %c%*c", &controle);
+            printf("\n");
+            if (strchr("nNsSyY", controle))
+                break;
+        }
+        if (controle == 'n' || controle == 'N')
+            break;
+    }
+    *nPessoas_ptr = nPessoas;
     return pessoas;
 }
 
-void EscrevePessoasBinario(tPessoa** pessoas, int nPessoas) {
-    FILE *arquivo;
+void EscrevePessoasBinario(tPessoa **pessoas, int nPessoas) {
     int pessoasGravadas = 0;
-    arquivo = fopen("pessoas.bin", "wb");
-
+    FILE *arquivo = fopen("pessoas.bin", "wb");
     if (arquivo == NULL) {
         return;
     }
-    
-    for ( int pessoa = 0; pessoa < nPessoas; pessoa++ ) {
-        if ( fwrite(pessoas[pessoa], sizeof(tPessoa), 1, arquivo) != 1 ) {
+    for (int i = 0; i < nPessoas; i++) {
+        if (fwrite(pessoas[i], sizeof(tPessoa), 1, arquivo) != 1) {
             fclose(arquivo);
             return;
         }
         pessoasGravadas++;
     }
-    
+
     printf("Pessoas gravadas: %d\n", pessoasGravadas);
     fclose(arquivo);
 }
 
 void ImprimePessoasBinario(int nPessoas) {
-    FILE *arquivo;
-    tPessoa* pessoaLida = (tPessoa*) malloc(sizeof(tPessoa));
-
-    arquivo = fopen("pessoas.bin", "rb");
-
+    tPessoa pessoa = { 0 };
+    FILE *arquivo = fopen("pessoas.bin", "rb");
     if (arquivo == NULL) {
-        free(pessoaLida);
         return;
     }
-
     int nPessoasLidas = 0;
-
-    for (int pessoa = 0; pessoa < nPessoas; pessoa++) {
-        if (fread(pessoaLida, sizeof(tPessoa), 1, arquivo) != 1) {
+    for (int i = 0; i < nPessoas; i++) {
+        if (fread(&pessoa, sizeof(tPessoa), 1, arquivo) != 1) {
             break;
         }
-
         nPessoasLidas++;
         printf("-----------------------------------\n");
-        printf("%s\n", pessoaLida->nome);
-        printf("%s\n", pessoaLida->dataNascimento);
-        printf("%s\n", pessoaLida->cpf);
+        printf("%s\n", pessoa.nome);
+        printf("%s\n", pessoa.dataNascimento);
+        printf("%s\n", pessoa.cpf);
         printf("-----------------------------------\n");
     }
 
     printf("Pessoas lidas: %d\n", nPessoasLidas);
-
-    free(pessoaLida);
     fclose(arquivo);
 }
 
-int main() {
+int main(void) {
     int nPessoas = 0;
-
-    tPessoa** pessoas = LePessoasInput(&nPessoas);
+    tPessoa **pessoas = LePessoasInput(&nPessoas);
     EscrevePessoasBinario(pessoas, nPessoas);
     ImprimePessoasBinario(nPessoas);
     DesalocaPessoas(pessoas, nPessoas);
-
     return 0;
 }
